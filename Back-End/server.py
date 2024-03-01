0b49ce94e89eb8a7404d1d41c2b153e45cbe0058
Diegho <dieghonm@gmail.com>
2024-03-01 04:05:12 -0300

    Altera codigo para multiplos clientes

--- Back-End/server.py
+++ Back-End/server.py
@@ -1,31 +1,75 @@
+import socket
 import tkinter as tk
-from server_socket import ServerSocket
-from server_interface import ServerInterface
+from threading import Thread
+from get_ip import get_ip_address
 
-class Server:
-    def __init__(self):
-        self.root = tk.Tk()
-        self.interface = ServerInterface(self.root, self.update_text)
-        self.socket = ServerSocket()
-        self.socket.start_server()
-        self.client_socket = None
-        self.receive_messages('texto do server para a interface')
-        self.root.mainloop()
+class ServerInterface:
+    def __init__(self, root):
+        self.root = root
+        self.port = 12345
+        self.root.title(f"Server ip:{get_ip_address()} / port:{self.port}\n")
 
-    def update_text(self, text):
-        print("Received text from child:", text)
+        self.messages_text = tk.Text(root)
+        self.messages_text.pack(padx=10, pady=10)
 
-    def receive_messages(self, text):
-        print('seilah2', text)
-        self.interface.receive_text(text)
-        # while True:
-        #     self.client_socket = self.socket.accept_connection()
-        #     message = self.socket.receive_message(self.client_socket)
-        #     if message.lower() == 'quit':
-        #         break
-        #     self.interface.receive_text(message)
-        #     self.socket.send_message(self.client_socket, "Message received")
-        #     self.socket.close_connection(self.client_socket)
+        self.entry = tk.Entry(root, width=30)
+        self.entry.pack(padx=10, pady=5)
+
+        self.send_button = tk.Button(root, text="Send", command=self.send_message)
+        self.send_button.pack(pady=5)
+
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind(('localhost', self.port))
+        self.server_socket.listen(5)  # Permitindo até 5 conexões pendentes
+
+        self.clients = []
+
+        self.accept_thread = Thread(target=self.accept_clients)
+        self.accept_thread.start()
+
+    def accept_clients(self):
+        while True:
+            client_socket, client_address = self.server_socket.accept()
+            print(f"Connected to {client_address}")
+            self.clients.append((client_socket, client_address))
+            receive_thread = Thread(target=self.receive_messages, args=(client_socket,))
+            receive_thread.start()
+
+    def send_message(self):
+        message = self.entry.get()
+        for client_socket, _ in self.clients:
+            client_socket.send(message.encode('utf-8'))
+        self.messages_text.insert(tk.END, f"Server: {message}\n")
+        self.entry.delete(0, tk.END)
+
+    def receive_messages(self, client_socket):
+        while True:
+            try:
+                data = client_socket.recv(1024).decode('utf-8')
+                if not data:
+                    break
+                self.messages_text.insert(tk.END, f"Client: {data}\n")
+                message = data
+                for client_socket, _ in self.clients:
+                    client_socket.send(message.encode('utf-8'))
+            except ConnectionAbortedError:
+                break
+
+
+
+def get_ip_address():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        ip_address = s.getsockname()[0]
+        s.close()
+        
+        return ip_address
+    except socket.error as e:
+        print(f"Erro ao obter o endereço IP: {e}")
+
 
 if __name__ == "__main__":
-    server = Server()
+    root = tk.Tk()
+    app = ServerInterface(root)
+    root.mainloop()
--- Front-End/client.py
+++ Front-End/client.py
@@ -1,20 +1,91 @@
-import threading
 import tkinter as tk
-from client_socket import ClientSocket
-from client_interface import ClientInterface
+import socket
+from threading import Thread
 
-def main():
-    host = '127.0.0.1'  # Altere para o endereço IP do servidor
-    port = 12345  # Altere para a porta em que o servidor está ouvindo
+class ClientInterface:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Client Interface")
 
-    # Criar o socket do cliente e conectar-se ao servidor
-    client_socket = ClientSocket(host, port)
-    client_socket.connect_to_server()
+        # Frame para conter os campos de entrada do IP, porta e nome de usuário
+        self.input_frame = tk.Frame(root)
+        self.input_frame.pack(padx=10, pady=5)
 
-    # Criar a interface do cliente
+        # Campo de entrada para o nome de usuário
+        self.username_label = tk.Label(self.input_frame, text="Username:")
+        self.username_label.pack(side=tk.LEFT)
+        self.username_entry = tk.Entry(self.input_frame, width=15)
+        self.username_entry.pack(side=tk.LEFT)
+
+        # Campo de entrada para o endereço IP
+        self.ip_label = tk.Label(self.input_frame, text="IP:")
+        self.ip_label.pack(side=tk.LEFT)
+        self.ip_entry = tk.Entry(self.input_frame, width=15)
+        self.ip_entry.pack(side=tk.LEFT)
+        self.ip_entry.insert(tk.END, "127.0.0.1")  # Sugestão de preenchimento com o endereço IP loopback padrão
+
+        # Campo de entrada para a porta
+        self.port_label = tk.Label(self.input_frame, text="Port:")
+        self.port_label.pack(side=tk.LEFT)
+        self.port_entry = tk.Entry(self.input_frame, width=10)
+        self.port_entry.pack(side=tk.LEFT)
+        self.port_entry.insert(tk.END, "12345")  # Sugestão de preenchimento com a porta padrão
+
+        self.connect_button = tk.Button(root, text="Connect", command=self.connect_to_server)
+        self.connect_button.pack(pady=5)
+
+        self.messages_text = tk.Text(root)
+        self.messages_text.pack(padx=10, pady=10)
+
+        self.entry = tk.Entry(root, width=30)
+        self.entry.pack(padx=10, pady=5)
+
+        self.send_button = tk.Button(root, text="Send", command=self.send_message)
+        self.send_button.pack(pady=5)
+
+        self.client_socket = None
+
+    def connect_to_server(self):
+        username = self.username_entry.get().upper()
+        ip = self.ip_entry.get()
+        port = int(self.port_entry.get())
+        
+        # Exibir os valores do IP e da porta como mostradores de texto
+        self.username_label.config(text=f"IP: {username}")
+        self.username_entry.pack_forget()
+        self.ip_label.config(text=f"IP: {ip}")
+        self.ip_entry.pack_forget()
+        self.port_label.config(text=f"Port: {port}")
+        self.port_entry.pack_forget()
+        
+        try:
+            self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.client_socket.connect((ip, port))
+            self.receive_thread = Thread(target=self.receive_messages)
+            self.receive_thread.start()
+            self.messages_text.insert(tk.END, "Connected to server\n")
+        except Exception as e:
+            self.messages_text.insert(tk.END, f"Error connecting to server: {e}\n")
+
+    def send_message(self):
+        if self.client_socket:
+            username = self.username_entry.get().upper()
+            message = self.entry.get()
+            full_message = f"{username}: {message}"
+            self.client_socket.send(full_message.encode('utf-8'))
+            self.entry.delete(0, tk.END)
+
+    def receive_messages(self):
+        while True:
+            try:
+                data = self.client_socket.recv(1024)
+                if not data:
+                    break
+                self.messages_text.insert(tk.END, f"{data.decode('utf-8')}\n")
+            except ConnectionAbortedError:
+                break
+
+if __name__ == "__main__":
     root = tk.Tk()
     app = ClientInterface(root)
     root.mainloop()
-
-if __name__ == "__main__":
-    main()
